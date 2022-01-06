b5f3bfa299ba7bda08c3b6e6e1cc72ca6b77bf57
mistarson <yeowuli2@naver.com>
2022-01-06 15:26:11 +0900

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.*;
+
+class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int count = Integer.parseInt(br.readLine());
+        int sum = 0;
+        String numStr = br.readLine();
+
+        for (int i = 0; i < numStr.length(); i++) {
+            sum += numStr.charAt(i) - '0';
+        }
+
+        System.out.println(sum);
+
+
+    }
+}
---
+++ src/backjoon/backjoon_10809.java
@@ -0,0 +1,27 @@
+package backjoon;
+
+import java.io.*;
+import java.util.Arrays;
+
+public class backjoon_10809 {
+
+    public static void main(String[] args) throws IOException {
+        
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        String S = br.readLine();
+        String[] splitStr = new String[S.length()];
+        String[] alphabet = {"a","b","c","e","f"}
+
+        for (int i = 0; i < S.length(); i++) {
+            splitStr[i] = String.valueOf(S.charAt(i));
+        }
+
+        for (int i = 0; i < splitStr.length; i++) {
+            for(int j=0; j<)
+        }
+
+
+    }
+}
