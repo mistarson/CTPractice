e9614b990eee3439cfe841e3f3dab3a108f361d2
mistarson <yeowuli2@naver.com>
2022-01-09 22:36:49 +0900

    백준 2908, 5622 추가

--- src/backjoon/backjoon_2908.java
+++ src/backjoon/backjoon_2908.java
@@ -3,14 +3,25 @@ package backjoon;
 import java.io.*;
 import java.util.StringTokenizer;
 
-public class backjoon_1152 {
+public class backjoon_2908 {
     public static void main(String[] args) throws IOException {
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
         String str = br.readLine();
         StringTokenizer st = new StringTokenizer(str);
         int count = st.countTokens();
-        bw.write(String.valueOf(count));
+        StringBuffer sb = new StringBuffer();
+        int number[] = new int[count];
+
+        for (int i = 0; i < count ; i++) {
+            sb.append(st.nextToken());
+            number [i] = Integer.parseInt(sb.reverse().toString());
+            sb.delete(0, sb.length());
+        }
+        bw.write(String.valueOf(Math.max(number[0],number[1])));
         bw.flush();
+
     }
 }
