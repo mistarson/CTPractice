e1e809f6c7fa924990185c50814c25b35d386038
mistarson <yeowuli2@naver.com>
2022-01-11 02:34:38 +0900

    백준 2292 추가

---
+++ src/backjoon/backjoon_2292.java
@@ -0,0 +1,23 @@
+package backjoon;
+
+import java.io.*;
+
+public class backjoon_2292 {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int bang = Integer.parseInt(br.readLine());
+        int n = 1;
+        if (bang > 1) {
+            int temp = 1;
+            while (bang > temp) {
+                temp += 6 * n;
+                n++;
+            }
+        }
+        bw.write(String.valueOf(n));
+        bw.flush();
+    }
+}
