f2045a49d659de22303148904104a1dd3720dd0c
mistarson <yeowuli2@naver.com>
2022-01-26 01:53:53 +0900

    백준 문제 추가

--- src/sds/p11438/Main.java
+++ src/sds/p11438/Main.java
@@ -1,43 +1,47 @@
 package sds.p11438;
 
 import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.StringTokenizer;
 
 public class Main {
 
-    static int N, K = 1;
-    static int[][] tree;
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static StringTokenizer st;
+    static int N, M;
+    static int maxHeight = 18; // 2^18 = 131,072, n의 최댓값 100,000
+    static List<Integer>[] graph;
+    static int[][] parent;
     static int[] depth;
+    static boolean[] visited;
 
     public static void main(String[] args) throws IOException {
 
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
         N = Integer.parseInt(br.readLine());
+
+        graph = new ArrayList[N+1];
         depth = new int[N + 1];
-
-        int cnt = 0;
-        while (N > K) {
-            K *= 2;
-            cnt++;
+        visited = new boolean[N + 1];
+        parent = new int[N + 1][maxHeight];
+        for (int i = 1; i <= N; i++) {
+            graph[i] = new ArrayList<>();
         }
 
-        tree = new int[K][N + 1];
-
-        while (true) {
-            String str = br.readLine();
-            if (str.length() == 0) {
-                break;
-            } else {
-                StringTokenizer st = new StringTokenizer(str);
-                int parent = Integer.parseInt(st.nextToken());
-                int child = Integer.parseInt(st.nextToken());
-
-
-
-            }
-
+        for (int i = 0; i < N - 1; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            graph[a].add(b);
+            graph[b].add(a);
         }
+
+
+
+
+
+
+
     }
 }
