a0d0684b2df43e97c54b7277707b459b5ed15ef5
mistarson <yeowuli2@naver.com>
2022-01-28 15:53:37 +0900

    백준 문제 추가

--- src/backjoon/backjoon_1759.java
+++ src/backjoon/backjoon_1759.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
 
 public class backjoon_1759 {
     static int L, C;
-    static char arr[],newarr[];
+    static char arr[], newarr[];
     static boolean visited[];
 
     public static void main(String[] args) throws IOException {
@@ -19,7 +19,7 @@ public class backjoon_1759 {
         newarr = new char[C];
         visited = new boolean[C];
 
-        for(int i = 0; i< C; i++) {
+        for (int i = 0; i < C; i++) {
             newarr[i] = sc.next().charAt(0);
         }
 
@@ -28,33 +28,34 @@ public class backjoon_1759 {
         combination(0, 0);
 
     }
-    static void combination(int start,int cnt) {
-        if(cnt== L) {
+
+    static void combination(int start, int cnt) {
+        if (cnt == L) {
 
             int v = 0;
             int cc = 0;
 
             StringBuilder sb = new StringBuilder();
 
-            for(int i = 0; i< C; i++) {
-                if(visited[i]) {
+            for (int i = 0; i < C; i++) {
+                if (visited[i]) {
                     sb.append(newarr[i]);
 
-                    if(newarr[i]=='a'||newarr[i]=='e'||newarr[i]=='i'||newarr[i]=='o'||newarr[i]=='u') {
+                    if (newarr[i] == 'a' || newarr[i] == 'e' || newarr[i] == 'i' || newarr[i] == 'o' || newarr[i] == 'u') {
                         v++;
-                    }else {
+                    } else {
                         cc++;
                     }
                 }
             }
-            if(v>=1 && cc>=2) System.out.println(sb);
+            if (v >= 1 && cc >= 2) System.out.println(sb);
 
         }
 
-        for(int i = start; i< C; i++) {
-            visited[i]=true;
-            combination(i+1,cnt+1);
-            visited[i]=false;
+        for (int i = start; i < C; i++) {
+            visited[i] = true;
+            combination(i + 1, cnt + 1);
+            visited[i] = false;
         }
     }
 }
