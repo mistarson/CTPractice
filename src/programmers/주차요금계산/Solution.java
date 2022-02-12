8499e0c0c574b74fbe8eb63a929b90a65ea174c2
mistarson <yeowuli2@naver.com>
2022-02-12 18:02:43 +0900

    프로그래머스 문제 추가

--- src/programmers/주차요금계산/Solution.java
+++ src/programmers/주차요금계산/Solution.java
@@ -4,9 +4,7 @@ import java.util.*;
 
 public class Solution {
 
-    static StringTokenizer st;
     public static void main(String[] args) {
-
 
         int[] fees = {180, 5000, 10, 600};
         String[] records = {"05:34 5961 IN", "06:00 0000 IN", "06:34 0000 OUT", "07:59 5961 OUT", "07:59 0148 IN", "18:59 0000 IN", "19:09 0148 OUT", "22:59 5961 IN", "23:00 5961 OUT"};
