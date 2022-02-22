bd7b8c5c756eb94e1612f8dffc1f0e994271db71
mistarson <yeowuli2@naver.com>
2022-02-22 22:41:18 +0900

    프로그래머스 문제 추가

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,55 @@
-import java.util.HashMap;
-
 public class Main {
     public static void main(String[] args) {
 
-        HashMap<String, Integer> map = new HashMap<>();
-        map.put("A", 1);
-        System.out.println(map);
+        Person p=new Person("2NE1");
 
-        map.put("A", 3);
-        System.out.println(map);
+        PersonService service = new PersonService();
+
+        service.pass1(p);
+
+        System.out.println(p.getName());
+
+        service.pass2(p);
+
+        System.out.println(p.getName());
+
+    }
+}
+class Person{
+
+    private String name;
+
+    public Person(String name) {
+
+        this.name = name;
 
     }
 
+    public String getName() {
+
+        return name;
+
+    }
+
+    public void setName(String name) {
+
+        this.name = name;
+
+    }
+
+}
+
+class PersonService {
+
+    public void pass1(Person p) {
+
+        p.setName("미쓰에이");
+
+    }
+
+    public void pass2(Person p) {
+
+        p = new Person("소녀시대");
+
+    }
 }
