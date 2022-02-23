32fea68aa1e9c48ba68ad9e5e450abffa642f715
mistarson <yeowuli2@naver.com>
2022-02-24 00:24:04 +0900

    프로그래머스 문제 추가

---
+++ src/programmers/숫자문자열과영단어/Solution.java
@@ -0,0 +1,22 @@
+package programmers.숫자문자열과영단어;
+
+public class Solution {
+    public static void main(String[] args) {
+
+        String s = "2three45sixseven";
+
+        System.out.println(solution(s));
+
+    }
+
+    static int solution(String s) {
+        String[] str = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < str.length; i++) {
+            s = s.replaceAll(str[i], String.valueOf(i));
+        }
+
+        return Integer.parseInt(s);
+    }
+}
