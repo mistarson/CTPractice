531005bcfb58c20d373ee57bf12e7daf09aa8a3e
mistarson <yeowuli2@naver.com>
2022-03-02 01:53:11 +0900

    프로그래머스 문제 추가

---
+++ src/programmers/level1/최소직사각형/Solution.java
@@ -0,0 +1,25 @@
+package programmers.level1.최소직사각형;
+
+public class Solution {
+    static int solution(int[][] sizes) {
+        int widthMax = Integer.MIN_VALUE;
+        int heightMax = Integer.MIN_VALUE;
+
+        for (int i = 0; i < sizes.length; i++) {
+            if (sizes[i][0] < sizes[i][1]) {
+                int temp = sizes[i][0];
+                sizes[i][0] = sizes[i][1];
+                sizes[i][1] = temp;
+            }
+            if (sizes[i][0] > widthMax) {
+                widthMax = sizes[i][0];
+            }
+            if (sizes[i][1] > heightMax) {
+                heightMax = sizes[i][1];
+            }
+        }
+
+
+        return widthMax * heightMax;
+    }
+}
