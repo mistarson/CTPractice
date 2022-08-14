7c6aa4ded73d8bf5e7c857607cd772b38aae58f2
mistarson <yeowuli2@naver.com>
2022-08-15 03:05:00 +0900

    백준 [2437] 저울 문제 추가

---
+++ src/BOJ/backjoon_2437.java
@@ -0,0 +1,33 @@
+package BOJ;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class backjoon_2437 {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+
+    public static void main(String[] args) throws IOException {
+        int N = Integer.parseInt(br.readLine());
+
+        int[] arr = new int[N];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr);
+
+        int sum = 0;
+        for (int i = 0; i < N - 1; i++) {
+            sum += arr[i];
+            if (sum + 1 < arr[i + 1]) {
+                System.out.println(sum + 1);
+                break;
+            }
+        }
+    }
+}
