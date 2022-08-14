b227b1f657fc716d4c7ac19d477df2c0ad3b9b14
mistarson <yeowuli2@naver.com>
2022-08-14 15:33:42 +0900

    프로그래머스 문자열압축 문제 복습

--- src/programmers/문자열압축/Solution.java
+++ src/programmers/문자열압축/Solution.java
@@ -5,7 +5,7 @@ import java.util.*;
 public class Solution {
 
     public static void main(String[] args) {
-        String s = "aabbaccc";
+        String s = "xababcdcdababcdcd";
 
         int solution = solution(s);
 
@@ -14,43 +14,37 @@ public class Solution {
     }
 
     static int solution(String s) {
-        int answer = 0;
-        int minLength = Integer.MAX_VALUE;
+        int length = s.length();
 
-        if (s.length() == 1) {
-            return 1;
-        }
-
-        for (int i = 1; i <= s.length() / 2; i++) {
+        int min = Integer.MAX_VALUE;
+        for (int i = 1; i <= length / 2; i++) {
             String originalStr = s.substring(0, i);
             StringBuilder sb = new StringBuilder();
-            int count = 1;
-            for (int j = 1; j < s.length() / i; j++) {
-                String comparisonStr = s.substring(j * i, j * i + i);
+            int cnt = 1;
+            for (int j = 1; j < length / i; j++) {
+                String compareStr = s.substring(j * i, j * i + i);
 
-                if (originalStr.equals(comparisonStr)) {
-                    count++;
+                if (originalStr.equals(compareStr)) {
+                    cnt++;
                 } else {
-                    if (count > 1) {
-                        sb.append(count);
-                        count = 1;
+                    if (cnt > 1) {
+                        sb.append(cnt);
+                        cnt = 1;
                     }
                     sb.append(originalStr);
-                    originalStr = comparisonStr;
+                    originalStr = compareStr;
                 }
             }
-
-            if (count > 1) {
-                sb.append(count);
+            if (cnt > 1) {
+                sb.append(cnt);
             }
             sb.append(originalStr);
 
-            if (s.length() % i != 0) {
-                sb.append(s.substring((s.length() / i) * i, s.length()));
+            if (length % i != 0) {
+                sb.append(s.substring((length / i) * i, length));
             }
-            minLength = Math.min(minLength, sb.length());
+            min = Math.min(min, sb.length());
         }
-
-        return answer = minLength;
+        return min;
     }
 }
