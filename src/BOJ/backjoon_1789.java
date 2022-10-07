b381634bb8190d3d713705b885090249921bbdcf
mistarson <yeowuli2@naver.com>
2022-10-07 21:28:16 +0900

    백준 [1789] 수들의합 문제 풀이

---
+++ src/BOJ/backjoon_1789.java
@@ -0,0 +1,24 @@
+package BOJ;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class backjoon_1789 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        long S = Long.parseLong(br.readLine());
+
+        long sum = 0;
+        int cnt = 0;
+        for (long i = 1; ; i++) {
+            if (sum > S) {
+                break;
+            }
+            sum += i;
+            cnt++;
+        }
+
+        System.out.println(cnt - 1);
+    }
+}
