db0cd635088e87000e6fd0049c54b620c0be6640
mistarson <yeowuli2@naver.com>
2022-10-09 20:53:02 +0900

    백준 [2206] 벽부수고이동하기 문제 성공 3차원 거리 배열을 사용한 풀이

--- src/BOJ/backjoon_2206.java
+++ src/BOJ/backjoon_2206.java
@@ -3,13 +3,13 @@ package BOJ;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class backjoon_2206 {
     static int N, M;
-    static int minDist = Integer.MAX_VALUE;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -25,15 +25,7 @@ public class backjoon_2206 {
             }
         }
 
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < M; j++) {
-                if (map[i][j] == 1) {
-                    map[i][j] = 0;
-                    minDist = Math.min(minDist, getMinDist(map));
-                    map[i][j] = 1;
-                }
-            }
-        }
+        int minDist = getMinDist(map);
 
         if (minDist == Integer.MAX_VALUE) {
             minDist = -1;
@@ -48,31 +40,58 @@ public class backjoon_2206 {
         int[] dr = {0, 1, 0, -1};
         int[] dc = {1, 0, -1, 0};
 
-        boolean[][] isVisited = new boolean[N][M];
+        int[][][] dist = new int[N][M][2];
+        for (int i = 0; i < dist.length; i++) {
+            for (int j = 0; j < dist[i].length; j++) {
+                Arrays.fill(dist[i][j], Integer.MAX_VALUE);
+            }
+        }
+        dist[0][0][0] = 1;
         Queue<Position> queue = new LinkedList<>();
-        queue.add(new Position(0, 0, 1));
-        isVisited[0][0] = true;
+        queue.add(new Position(0, 0, 0, 1));
 
-        int minDist = Integer.MAX_VALUE;
         while (!queue.isEmpty()) {
             Position current = queue.poll();
 
-            if (current.r == N - 1 && current.c == M - 1) {
-                minDist = current.dist;
-                break;
+            if (dist[current.r][current.c][current.crashWall] < current.dist) {
+                continue;
             }
 
             for (int i = 0; i < 4; i++) {
                 int nr = current.r + dr[i];
                 int nc = current.c + dc[i];
 
-                if (inRange(nr, nc) && !isVisited[nr][nc] && map[nr][nc] == 0) {
-                    isVisited[nr][nc] = true;
-                    queue.add(new Position(nr, nc, current.dist + 1));
+                // 격자를 벗어나지 않고
+                if (inRange(nr, nc)) {
+                    // 벽을 부순 적이 있다면
+                    if (current.crashWall == 1) {
+                        if (map[nr][nc] == 0) {
+                            if (current.dist + 1 < dist[nr][nc][1]) {
+                                dist[nr][nc][1] = current.dist + 1;
+                                queue.add(new Position(nr, nc, 1, current.dist + 1));
+                            }
+                        }
+                    }
+                    //벽을 부순 적이 없다면
+                    else {
+                        // 다음 위치가 벽이라면
+                        if (map[nr][nc] == 1) {
+                            if (current.dist + 1 < dist[nr][nc][1]) {
+                                dist[nr][nc][1] = current.dist + 1;
+                                queue.add(new Position(nr, nc, 1, current.dist + 1));
+                            }
+                        } else {
+                            if (current.dist + 1 < dist[nr][nc][0]) {
+                                dist[nr][nc][0] = current.dist + 1;
+                                queue.add(new Position(nr, nc, 0, current.dist + 1));
+                            }
+                        }
+                    }
                 }
             }
         }
-        return minDist;
+
+        return Math.min(dist[N - 1][M - 1][0], dist[N - 1][M - 1][1]);
     }
 
     static boolean inRange(int r, int c) {
@@ -82,11 +101,13 @@ public class backjoon_2206 {
     static class Position {
         int r;
         int c;
+        int crashWall;
         int dist;
 
-        public Position(int r, int c, int dist) {
+        public Position(int r, int c, int crashWall, int dist) {
             this.r = r;
             this.c = c;
+            this.crashWall = crashWall;
             this.dist = dist;
         }
     }
