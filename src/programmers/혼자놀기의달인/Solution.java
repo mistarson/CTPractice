d8c99e887ce1ecd78b87b22ca2da2b3386a7b2ba
mistarson <yeowuli2@naver.com>
2022-10-19 01:39:06 +0900

    프로그래머스 혼자놀기의달인 문제 풀이

--- src/programmers/혼자놀기의달인/Solution.java
+++ src/programmers/혼자놀기의달인/Solution.java
@@ -1,46 +1,51 @@
 package programmers.혼자놀기의달인;
 
 import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 public class Solution {
-    public int solution(int[] cards) {
-        int answer = 0;
 
-        int[] score = new int[101];
-        Arrays.fill(score, -1);
+    public int solution(int[] cards) {
+
+        List<Integer> scoreGroupByBox = new ArrayList<>();
+        boolean[] isOpened = new boolean[cards.length + 1];
+        int[] scores = new int[101];
         for (int i = 0; i < cards.length; i++) {
-            if (score[cards[i]] == -1) {
-                List<List<Integer>> cardListGroupByBox = new ArrayList<>();
-                answer = Math.max(answer, getScore(0, cards[i], score, cards, cardListGroupByBox));
+            if (!isOpened[cards[i]]) {
+                int score = getScore(cards[i], scores, cards, isOpened);
+                if (score != 0) {
+                    scoreGroupByBox.add(score);
+                } else {
+                    return 0;
+                }
             }
         }
-        return answer;
+
+        Collections.sort(scoreGroupByBox, Collections.reverseOrder());
+
+        return scoreGroupByBox.get(0) * scoreGroupByBox.get(1);
     }
 
-    static int getScore(int idx, int card, int[] score, int[] cards, List<List<Integer>> cardListGroupByBox) {
+    static int getScore(int card, int[] scores, int[] cards, boolean[] isOpened) {
 
-        cardListGroupByBox.add(new ArrayList<>());
+        List<Integer> cardList = new ArrayList<>();
 
-        while (card <= cards.length) {
-            cardListGroupByBox.get(idx).add(card);
-            card = cards[card];
+        while (card <= cards.length && !isOpened[card]) {
+            isOpened[card] = true;
+            cardList.add(card);
+            card = cards[card - 1];
         }
 
-        if (cardListGroupByBox.get(idx).size() == cards.length) {
+        if (cardList.size() == cards.length) {
             return 0;
         }
 
-        for (int cardNum : cardListGroupByBox.get(idx)) {
-            score[cardNum] = cardListGroupByBox.get(idx).size();
+        for (int cardNum : cardList) {
+            scores[cardNum] = cardList.size();
         }
 
-        for (int i = 0; i < cards.length; i++) {
-            if (score[cards[i]] != -1) {
-                getScore(idx + 1, cards[i], score, cards, cardListGroupByBox);
-            }
-        }
+        return cardList.size();
 
     }
 }
