790082d3c082272fbb96a5b9ecfe9b0f9ce8f09a
sonchanghyeon <cson90563@gmail.com>
2026-01-08 21:31:47 +0900

    프로그래머스 전화번호목록 문제 재풀이

--- src/programmers/전화번호목록/Solution.java
+++ src/programmers/전화번호목록/Solution.java
@@ -1,36 +1,24 @@
 package programmers.전화번호목록;
 
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class Solution {
 
     public static void main(String[] args) {
 
-        String[] phone_book = {"119", "97674223", "1195524421"};
+        String[] phone_book = {"119", "97674223", "976742231195524421"};
 
-        solution(phone_book);
+        System.out.println(solution(phone_book));
     }
 
     static boolean solution(String[] phone_book) {
-        boolean answer = true;
+        Arrays.sort(phone_book);
 
-        Map<String, Integer> pbMap = new HashMap<>();
-
-        for (int i = 0; i < phone_book.length; i++) {
-            pbMap.put(phone_book[i], i);
-        }
-
-        for (int i = 0; i < phone_book.length; i++) {
-            for (int j = 0; j < phone_book[i].length(); j++) {
-                String substring = phone_book[i].substring(0, j); // substring(end 전까지 그니까 end-1까지 나옴)
-                if (pbMap.containsKey(substring)) {
-                    return answer = false;
-                }
+        for (int i = 0; i < phone_book.length - 1; i++) {
+            if (phone_book[i + 1].startsWith(phone_book[i])) {
+                return false;
             }
         }
-
-        return answer;
+        return true;
     }
 }
